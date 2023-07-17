670486963d26f83ed8520d4017b9c3ae92dcecd5
NK.Shien_Home <shien091091@gmail.com>
2023-07-18 00:23:39 +0800

    1.Test Code Refactory

--- Assets/Scripts/Editor/PlayerModel/PlayerModelTest.cs
+++ Assets/Scripts/Editor/PlayerModel/PlayerModelTest.cs
@@ -20,43 +20,65 @@ namespace GameCore.Tests.Player
         //角色不移動
         public void no_move()
         {
-            inputAxisController.GetHorizontalAxis().Returns(0);
-            inputAxisController.GetVerticalAxis().Returns(0);
+            GivenMoveAxis(0, 0);
             Vector2 moveVector = playerModel.UpdateMove(1, 1);
-            Assert.IsTrue(moveVector == Vector2.zero);
+            ShouldNoMove(moveVector);
         }
 
         [Test]
         //角色水平移動
         public void horizontal_move()
         {
-            inputAxisController.GetHorizontalAxis().Returns(0.6f);
-            inputAxisController.GetVerticalAxis().Returns(0);
+            GivenMoveAxis(0.6f, 0);
             Vector2 moveVector = playerModel.UpdateMove(1, 1);
-            Assert.IsTrue(moveVector.x > 0);
-            Assert.IsTrue(moveVector.y == 0);
+            ShouldMoveRight(moveVector);
         }
 
         [Test]
         //角色垂直移動
         public void vertical_move()
         {
-            inputAxisController.GetHorizontalAxis().Returns(0);
-            inputAxisController.GetVerticalAxis().Returns(0.7f);
+            GivenMoveAxis(0, 0.7f);
             Vector2 moveVector = playerModel.UpdateMove(1, 1);
-            Assert.IsTrue(moveVector.y > 0);
-            Assert.IsTrue(moveVector.x == 0);
+            ShouldMoveUp(moveVector);
         }
 
         [Test]
         //角色水平+垂直移動
         public void horizontal_vertical_move()
         {
-            inputAxisController.GetHorizontalAxis().Returns(-0.7f);
-            inputAxisController.GetVerticalAxis().Returns(-0.8f);
+            GivenMoveAxis(-0.7f, -0.8f);
             Vector2 moveVector = playerModel.UpdateMove(1, 1);
+            ShouldMoveLeftAndDown(moveVector);
+        }
+
+        private void GivenMoveAxis(float horizontalAxis, float verticalAxis)
+        {
+            inputAxisController.GetHorizontalAxis().Returns(horizontalAxis);
+            inputAxisController.GetVerticalAxis().Returns(verticalAxis);
+        }
+
+        private void ShouldMoveLeftAndDown(Vector2 moveVector)
+        {
             Assert.IsTrue(moveVector.x < 0);
             Assert.IsTrue(moveVector.y < 0);
+        }
+
+        private void ShouldMoveUp(Vector2 moveVector)
+        {
+            Assert.IsTrue(moveVector.y > 0);
+            Assert.IsTrue(moveVector.x == 0);
+        }
+
+        private void ShouldMoveRight(Vector2 moveVector)
+        {
+            Assert.IsTrue(moveVector.x > 0);
+            Assert.IsTrue(moveVector.y == 0);
+        }
+
+        private void ShouldNoMove(Vector2 moveVector)
+        {
+            Assert.IsTrue(moveVector == Vector2.zero);
         }
     }
 }
