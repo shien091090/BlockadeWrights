70e749dd4a5b8b1816a47df8654ab1698cbb74ea
NK.Shien_Home <shien091091@gmail.com>
2023-07-30 17:43:15 +0800

    Test Scripts Refactor

--- Assets/Scripts/Editor/Monster/MonsterSpawnerModelTest.cs
+++ Assets/Scripts/Editor/Monster/MonsterSpawnerModelTest.cs
@@ -6,27 +6,56 @@ namespace GameCore.Tests.Monster
 {
     public class MonsterSpawnerModelTest
     {
+        private IAttackWave attackWave;
+        private MonsterSpawnerModel monsterSpawnerModel;
+        private Action onSpawnMonster;
+
+        [SetUp]
+        public void Setup()
+        {
+            attackWave = Substitute.For<IAttackWave>();
+            monsterSpawnerModel = new MonsterSpawnerModel(attackWave);
+
+            onSpawnMonster = Substitute.For<Action>();
+            monsterSpawnerModel.OnSpawnMonster += onSpawnMonster;
+        }
+
         [Test]
         //目前波次已達產怪上限
         public void current_wave_is_spawn_completed()
         {
-            IAttackWave attackWave = Substitute.For<IAttackWave>();
-            attackWave.GetMaxSpawnCount.Returns(5);
-            attackWave.GetCurrentSpawnCount.Returns(5);
-            
-            MonsterSpawnerModel monsterSpawnerModel = new MonsterSpawnerModel(attackWave);
-            
-            Action onSpawnMonster = Substitute.For<Action>();
-            monsterSpawnerModel.OnSpawnMonster += onSpawnMonster;
-            
-            Assert.AreEqual(false, monsterSpawnerModel.CanSpawnNext);
-            
+            GivenMaxSpawnCount(5);
+            GivenCurrentSpawnCount(5);
+
             monsterSpawnerModel.Spawn();
-            onSpawnMonster.DidNotReceive().Invoke();
-            
+
+            ShouldCanSpawnNext(false);
+            ShouldTriggerSpawnEvent(0);
         }
-        
+
+        private void GivenCurrentSpawnCount(int currentSpawnCount)
+        {
+            attackWave.GetCurrentSpawnCount.Returns(currentSpawnCount);
+        }
+
+        private void GivenMaxSpawnCount(int maxSpawnCount)
+        {
+            attackWave.GetMaxSpawnCount.Returns(maxSpawnCount);
+        }
+
+        private void ShouldTriggerSpawnEvent(int triggerTimes)
+        {
+            if (triggerTimes == 0)
+                onSpawnMonster.DidNotReceive().Invoke();
+            else
+                onSpawnMonster.Received(triggerTimes).Invoke();
+        }
+
+        private void ShouldCanSpawnNext(bool expectedCanSpawnNext)
+        {
+            Assert.AreEqual(expectedCanSpawnNext, monsterSpawnerModel.CanSpawnNext);
+        }
+
         //目前波次未達產怪上限
     }
-
 }
