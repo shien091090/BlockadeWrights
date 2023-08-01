b46ed1783498165b84b7395cde5057c44a463e2f
NK.Shien_Home <shien091091@gmail.com>
2023-08-01 21:35:40 +0800

    Add MonsterModelTest.cs

---
+++ Assets/Scripts/Editor/Monster/MonsterModelTest.cs
@@ -0,0 +1,11 @@
+namespace GameCore.Tests.Monster
+{
+    public class MonsterModelTest
+    {
+        //無行走路線, 不移動
+        //行走路徑僅起點和終點, 往終點移動
+        //行走路徑有多個點, 從起點往第二個點移動
+        //行走路徑有多個點, 從中間點往下一個點移動
+        //移動至終點, 破壞主堡
+    }
+}
