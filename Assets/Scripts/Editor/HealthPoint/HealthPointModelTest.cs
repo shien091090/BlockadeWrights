6c9490ffc1f10abb1bf83ecb59e393d901091cb8
NK.Shien_Home <shien091091@gmail.com>
2023-08-02 23:39:55 +0800

    新增HealthPointModelTest.cs

---
+++ Assets/Scripts/Editor/HealthPoint/HealthPointModelTest.cs
@@ -0,0 +1,12 @@
+namespace GameCore.Tests.HealthPoint
+{
+    public class HealthPointModelTest
+    {
+        //HP上限為0
+        //扣血
+        //扣血至HP歸0
+        //扣血超出目前HP
+        //補血
+        //補血超出HP上限
+    }
+}
