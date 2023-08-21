6000646699ed09a256d6fb9e00da65d29e9841b9
NK.Shien_Home <shien091091@gmail.com>
2023-08-21 22:19:44 +0800

    Refactor Test Scripts

--- Assets/Scripts/Editor/Timer/TimerModelTest.cs
+++ Assets/Scripts/Editor/Timer/TimerModelTest.cs
@@ -4,30 +4,46 @@ namespace GameCore.Tests.Timer
 {
     public class TimerModelTest
     {
+        private TimerModel timerModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            timerModel = new TimerModel();
+        }
+
         [Test]
         //計時器未設置時限
         public void no_count_down_time_setting()
         {
-            TimerModel timerModel = new TimerModel();
             timerModel.StartCountDown(0);
             timerModel.UpdateCountDownTime(1);
-            
-            Assert.AreEqual(false, timerModel.IsTimerPlaying);
-            Assert.AreEqual(0, timerModel.CurrentTime);
+
+            ShouldTimerPlaying(false);
+            CurrentTimeShouldBe(0);
         }
 
         [Test]
         //時限設置為負數
         public void count_down_time_setting_is_negative()
         {
-            TimerModel timerModel = new TimerModel();
             timerModel.StartCountDown(-1);
             timerModel.UpdateCountDownTime(1);
-            
-            Assert.AreEqual(false, timerModel.IsTimerPlaying);
-            Assert.AreEqual(0, timerModel.CurrentTime);
+
+            ShouldTimerPlaying(false);
+            CurrentTimeShouldBe(0);
         }
-        
+
+        private void CurrentTimeShouldBe(int expectedCurrentTime)
+        {
+            Assert.AreEqual(expectedCurrentTime, timerModel.CurrentTime);
+        }
+
+        private void ShouldTimerPlaying(bool expectedIsPlaying)
+        {
+            Assert.AreEqual(expectedIsPlaying, timerModel.IsTimerPlaying);
+        }
+
         //刷新倒數時間
         //刷新倒數時間結束
     }
