9315dfa257ba985e5a3d88aaf64ef7354f62ed23
NK.Shien_Home <shien091091@gmail.com>
2023-08-21 22:16:51 +0800

    1.Add Test Case : 時限設置為負數

--- Assets/Scripts/Editor/Timer/TimerModelTest.cs
+++ Assets/Scripts/Editor/Timer/TimerModelTest.cs
@@ -9,14 +9,25 @@ namespace GameCore.Tests.Timer
         public void no_count_down_time_setting()
         {
             TimerModel timerModel = new TimerModel();
-            timerModel.StartCountDown();
+            timerModel.StartCountDown(0);
             timerModel.UpdateCountDownTime(1);
             
             Assert.AreEqual(false, timerModel.IsTimerPlaying);
             Assert.AreEqual(0, timerModel.CurrentTime);
         }
 
+        [Test]
         //時限設置為負數
+        public void count_down_time_setting_is_negative()
+        {
+            TimerModel timerModel = new TimerModel();
+            timerModel.StartCountDown(-1);
+            timerModel.UpdateCountDownTime(1);
+            
+            Assert.AreEqual(false, timerModel.IsTimerPlaying);
+            Assert.AreEqual(0, timerModel.CurrentTime);
+        }
+        
         //刷新倒數時間
         //刷新倒數時間結束
     }
--- Assets/Scripts/Timer/TimerModel.cs
+++ Assets/Scripts/Timer/TimerModel.cs
@@ -2,11 +2,20 @@ namespace GameCore
 {
     public class TimerModel
     {
-        public bool IsTimerPlaying { get; set; }
-        public float CurrentTime { get; set; }
+        public bool IsTimerPlaying { get; private set; }
+        public float CurrentTime { get; private set; }
 
-        public void StartCountDown()
+        public void StartCountDown(float countDownTime)
         {
+            if (countDownTime <= 0)
+            {
+                CurrentTime = 0;
+                IsTimerPlaying = false;
+                return;
+            }
+            
+            CurrentTime = countDownTime;
+            IsTimerPlaying = true;
         }
 
         public void UpdateCountDownTime(int deltaTime)
