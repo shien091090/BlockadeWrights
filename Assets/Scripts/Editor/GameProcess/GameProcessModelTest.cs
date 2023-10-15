0b19c52bccec050cc607199d9b462bc7ec96aa6b
NK.Shien_Home <shien091091@gmail.com>
2023-10-15 11:04:29 +0800

    1.GameProcessModelTest 列TestCase

---
+++ Assets/Scripts/Editor/GameProcess/GameProcessModelTest.cs
@@ -0,0 +1,12 @@
+namespace GameCore.Tests.GameProcess
+{
+    public class GameProcessModelTest
+    {
+        //第一波產怪需等待時間, 顯示倒數計時
+        //第一波產怪開始時, 波次從0變1
+        //等待第二波產怪, 不顯示倒數計時
+        //第二波產怪開始時, 波次從1變2
+        //所有怪物死亡, 顯示勝利畫面
+        //主堡被破壞, 顯示失敗畫面
+    }
+}
