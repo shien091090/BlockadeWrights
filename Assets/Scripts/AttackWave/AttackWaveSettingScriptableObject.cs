1b701d29eb3bf6931de473f4554af0eefb1f77ba
NK.Shien_Home <shien091091@gmail.com>
2023-10-16 00:08:45 +0800

    1.波次設定會按照開始時間由先到後排序

--- Assets/Scripts/AttackWave/AttackWaveSettingScriptableObject.cs
+++ Assets/Scripts/AttackWave/AttackWaveSettingScriptableObject.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -6,13 +7,15 @@ namespace GameCore
     public class AttackWaveSettingScriptableObject : SerializedScriptableObject, IAttackWaveSetting
     {
         [SerializeField] private AttackWaveSetting[] attackWaveSettings;
-        
+
         public AttackWave[] GetAttackWaves()
         {
             AttackWave[] attackWaves = new AttackWave[attackWaveSettings.Length];
+            AttackWaveSetting[] orderAttackWaveSettings = attackWaveSettings.OrderBy(x => x.StartTimeSecond).ToArray();
+
             for (int i = 0; i < attackWaves.Length; i++)
             {
-                AttackWaveSetting attackWaveSetting = attackWaveSettings[i];
+                AttackWaveSetting attackWaveSetting = orderAttackWaveSettings[i];
                 attackWaves[i] = new AttackWave(
                     attackWaveSetting.SpawnIntervalSecond,
                     attackWaveSetting.GetMonsterOrderList(),
